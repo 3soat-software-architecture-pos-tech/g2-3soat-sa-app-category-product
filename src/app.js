e4613743c06c2e542046b5b5253cbb77dc309e75
VALDEIR <valdeir_014@hotmail.com>
2024-08-07 21:41:24 -0300

    Ajuste filas no canvas

--- src/app.js
+++ src/app.js
@@ -4,15 +4,41 @@ import sendMessage from "./addMsgToFila.js";
 import receiveMessages from "./filaMsgReservaProdutos.js";
 import receiveMessagesDevolvidos from "./filaMsgDevolucao.js";
 
+//const nonce = crypto.randomBytes(16).toString('base64');
+//console.log('Generated nonce:', nonce);
+
 const app = express();
+// Middleware para gerar nonce (ou você pode gerar isso de outra maneira)
+/*app.use((req, res, next) => {
+  res.locals.nonce = crypto.randomBytes(16).toString('base64'); // Exemplo de geração de nonce
+  next();
+});
+
+// Configuração do Helmet com CSP
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'none'"],
+      scriptSrc: [`'self'`, (req, res) => `'nonce-${res.locals.nonce}'`, "https://trusted-scripts.com"],
+      styleSrc: [`'self'`, (req, res) => `'nonce-${res.locals.nonce}'`, "https://trusted-styles.com"],
+      imgSrc: ["'self'", "https://trusted-images.com"],
+      fontSrc: ["'self'", "https://trusted-fonts.com"],
+      connectSrc: ["'self'", "https://api.trusted.com"],
+      frameSrc: ["'self'", "https://trusted-frames.com"],
+      frameAncestors: ["'self'"],
+      formAction: ["'self'"],
+    },
+  },
+}));*/
+//app.use(helmet());
 app.use(express.json());
 
 routes(app, express);
 
 
 // SQS processo de recebimento de mensagens
-//--receiveMessages();
-//--receiveMessagesDevolvidos();
+receiveMessages();
+receiveMessagesDevolvidos();
 //Send Message
 const paramsMsgReservaProdutos = {
   //MessageBody: 'Hello from Node.js!',
@@ -31,7 +57,7 @@ const paramsMsgReservaProdutos = {
   MessageGroupId:'1',
   MessageDeduplicationId:'11'
 };
-//--sendMessage(paramsMsgReservaProdutos);
+sendMessage(paramsMsgReservaProdutos);
 
 const paramsMsgDevolveProdutos = {
   //MessageBody: 'Hello from Node.js!',
@@ -50,6 +76,6 @@ const paramsMsgDevolveProdutos = {
   MessageGroupId:'1',
   MessageDeduplicationId:'1'
 };
-//--sendMessage(paramsMsgDevolveProdutos);
+sendMessage(paramsMsgDevolveProdutos);
 
 export default app;
